febc3aa1647f099c58aeac91f54b2b7b070d1cf1
philkunz <github@philkunz.com>
2016-08-28 14:51:04 +0200

    start key Value store

---
+++ ts/npmextra.classes.keyValueStore.ts
@@ -0,0 +1,34 @@
+import * as plugins from "./npmextra.plugins";
+
+type keyValueStoreTypes = "path" | "gitProject"
+
+/**
+ * reads a keyValueFile from disk
+ */
+let kvRead = () => {
+
+};
+
+/**
+ * writes a key value file to disk
+ */
+let kvWrite = () => {
+
+};
+
+/**
+ * wipes a key value store from disk
+ */
+let kevWipe = () => {
+
+}
+
+export class KeyValueStore {
+    constructor(optionsArg:{
+        type: keyValueStoreTypes
+    }){
+
+    };
+
+
+}
