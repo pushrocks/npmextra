513bb686ef6affa6d1e93bc7f1809f4c8574b16d
PhilKunz <github@philkunz.com>
2016-09-24 21:49:53 +0200

    update

--- ts/npmextra.classes.keyvaluestore.ts
+++ ts/npmextra.classes.keyvaluestore.ts
@@ -1,32 +1,67 @@
 import * as plugins from './npmextra.plugins'
+import * as paths from './npmextra.paths'
 
-export type keyValueStoreTypes = 'path' | 'gitProject'
+export type keyValueStoreTypes = 'path' | 'gitProject' | 'customString'
 
 
 export class KeyValueStore {
-    constructor(optionsArg: {
-        type: keyValueStoreTypes
-    }) {
-
+    type: string // the type of the kvStore
+    identity: string // the identity of the kvStore
+    filePath: string // the filePath of the kvStore
+    constructor(typeArg: keyValueStoreTypes, identityStringArg?: string) {
+        // set kvStoreType
+        this.type = typeArg
+        this.initIdentity(identityStringArg)
+        this.initFilePath()
     }
+
     /**
- * reads a keyValueFile from disk
- */
-    kvRead() {
+     * reads a keyValueFile from disk
+     */
+    read() {
 
     }
 
     /**
      * writes a key value file to disk
      */
-    kvWrite() {
+    write() {
 
     }
 
     /**
      * wipes a key value store from disk
      */
-    kevWipe() {
+    wipe() {
 
+    }
+
+    /**
+     * updates a value
+     */
+    update() {
+
+    }
+
+    /**
+     * computes the identity
+     */
+    private initIdentity(identityStringArg: string){
+
+    }
+    
+    /**
+     * computes the filePath 
+     */
+    private initFilePath() {
+        let baseDir: string
+        if (this.type === 'customString') {
+            baseDir = paths.kvCusomDir
+        } else if (this.type === 'gitProject') {
+            baseDir = paths.kvGitDir
+        } else if (this.type === 'path') {
+            baseDir = paths.kvPathDir
+        }
+        this.filePath = plugins.path.join(baseDir, this.identity + '.json')
     }
 }
--- ts/npmextra.paths.ts
+++ ts/npmextra.paths.ts
@@ -4,5 +4,11 @@ import * as plugins from './npmextra.plugins'
 export let cwd = process.cwd()
 export let packageDir = plugins.path.join(__dirname,'../')
 
+// keyValueStore specific
+export let kvBase = '~/.npmextra/kv'
+export let kvCusomDir = plugins.path.join(kvBase, 'custom')
+export let kvGitDir = plugins.path.join(kvBase, 'git')
+export let kvPathDir = plugins.path.join(kvBase, 'path')
+
 // files
 export let configFile = plugins.path.join(cwd,'npmextra.json')
